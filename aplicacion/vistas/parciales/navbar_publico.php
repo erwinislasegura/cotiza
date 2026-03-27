043510846663414eab059054abef7b11622d844a
erwinislasegura <erwin.2785@gmail.com>
2026-03-27 03:26:15 -0300

    Mejora gráficos comparativos y de ganancias con estilo dashboard

--- aplicacion/vistas/parciales/navbar_publico.php
+++ aplicacion/vistas/parciales/navbar_publico.php
@@ -1,13 +1,17 @@
-<nav class="navbar navbar-expand-lg bg-white border-bottom sticky-top">
-  <div class="container"><a class="navbar-brand fw-semibold" href="<?= e(url('/')) ?>">CotizaPro</a>
+<nav class="navbar navbar-expand-lg bg-white border-bottom sticky-top public-navbar">
+  <div class="container">
+    <a class="navbar-brand fw-semibold" href="<?= e(url('/')) ?>">CotizaPro</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#n"><span class="navbar-toggler-icon"></span></button>
     <div class="collapse navbar-collapse" id="n">
-      <ul class="navbar-nav ms-auto gap-2 small">
+      <ul class="navbar-nav ms-auto gap-1 small align-items-lg-center">
+        <li class="nav-item"><a class="nav-link" href="<?= e(url('/')) ?>">Inicio</a></li>
         <li class="nav-item"><a class="nav-link" href="<?= e(url('/caracteristicas')) ?>">Características</a></li>
         <li class="nav-item"><a class="nav-link" href="<?= e(url('/planes')) ?>">Planes</a></li>
+        <li class="nav-item"><a class="nav-link" href="<?= e(url('/preguntas-frecuentes')) ?>">FAQ</a></li>
         <li class="nav-item"><a class="nav-link" href="<?= e(url('/contacto')) ?>">Contacto</a></li>
-        <li class="nav-item"><a class="btn btn-outline-primary btn-sm" href="<?= e(url('/iniciar-sesion')) ?>">Iniciar sesión</a></li>
-        <li class="nav-item"><a class="btn btn-primary btn-sm" href="<?= e(url('/registro')) ?>">Comenzar ahora</a></li>
+        <li class="nav-item"><a class="btn btn-outline-primary btn-sm w-100" href="<?= e(url('/iniciar-sesion')) ?>">Iniciar sesión</a></li>
+        <li class="nav-item"><a class="btn btn-primary btn-sm w-100" href="<?= e(url('/registro')) ?>">Crear cuenta empresarial</a></li>
       </ul>
-    </div></div>
+    </div>
+  </div>
 </nav>
